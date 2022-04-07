dee32abc7e1db15dc4bb8829c85af13447978d48
Amin-sabir <chamchamal90@yahoo.com>
2022-04-07 21:37:54 +0300

    first commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img src="/img/IMG.jpg" alt="">
+    <h1>Name : Amin Muhammad Sabr<h1>
+    <h1> Age  : 31 Years Old<h1>
+    <h1>Stage : Third Stage <h1>
+    
+    
+</body>
+</html>
